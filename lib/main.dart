39a1bc10d20d8dec011610515106432b38ea09de
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-06-28 09:56:09 -0500

    shared preferences finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:travelappg12/pages/home_page.dart';
+import 'package:travelappg12/pages/shared_preferences_page.dart';
 
 void main() {
-  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+  runApp(
+    MaterialApp(
+      home: SharedPreferencesPage(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
--- lib/pages/shared_preferences_page.dart
+++ lib/pages/shared_preferences_page.dart
@@ -1,25 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class SharedPreferencesPage extends StatefulWidget {
+  const SharedPreferencesPage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<SharedPreferencesPage> createState() => _SharedPreferencesPageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _SharedPreferencesPageState extends State<SharedPreferencesPage> {
   int contador = 0;
+  String user = "";
 
   void guardarContador() async {
     final prefs = await SharedPreferences.getInstance();
+    // GUARDANDO EL CONTADOR
     await prefs.setInt("contador", contador);
+    //GUARDANDO MÁS INFORMACIÓN
+    prefs.setString("username", "JhonnyGal");
+    prefs.setDouble("heigyh", 1.75);
+    prefs.setBool("isLoggedIn", true);
+    prefs.setStringList("favorites", ["Flutter", "Dart", "Developer"]);
   }
 
   void leerContador() async {
     final prefs = await SharedPreferences.getInstance();
     int? contadorAux = prefs.getInt("contador");
+    user = prefs.getString("username") ?? "-";
     contador = contadorAux ?? 0;
+
     setState(() {});
     // if (contadorAux == null) {
     //   contador = 0;
@@ -41,11 +50,13 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text(user, style: TextStyle(fontSize: 50)),
             Text(contador.toString(), style: TextStyle(fontSize: 50)),
             ElevatedButton(
               onPressed: () {
                 contador++;
                 guardarContador();
+
                 setState(() {});
               },
               child: Text("añadir 1"),
